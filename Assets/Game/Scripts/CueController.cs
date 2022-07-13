067dc0b3167811a842a046ed2a262253785e543a
PavloKPO <pavlokolykhanov@gmail.com>
2022-07-13 11:07:09 +0300

    4.3 Working with vectors(7)

--- Assets/Game/Scripts/CueController.cs
+++ Assets/Game/Scripts/CueController.cs
@@ -2,27 +2,24 @@ using UnityEngine;
 
 public class CueController : MonoBehaviour
 {
-    [SerializeField] private GameObject _ball;
-    [SerializeField] private GameObject _cue;
+    [SerializeField] private Rigidbody _ball;
+    [SerializeField] private Transform _cue;
     [SerializeField] private GameObject _cueSprite;
     [SerializeField] private Camera _camera;
         
-    private Rigidbody _rbBall;
+   
 
     [SerializeField] private float _force;
 
 
-    private void Start()
-    {        
-        _rbBall = _ball.GetComponent<Rigidbody>();
-    }
+    
 
     private void Update()
     {
-        if(_rbBall.velocity.magnitude < 0.1f)
+        if(_ball.velocity.magnitude < 0.1f)
         {
             var ballPos = _ball.transform.position;
-            _cue.transform.position = ballPos;
+            _cue.position = ballPos;
             
             _cueSprite.SetActive(true);
             CueRotationAndHitTheBalls();            
@@ -44,13 +41,12 @@ public class CueController : MonoBehaviour
 
         var ballPos = _ball.transform.position;
         var vectorForRotation = new Vector3(worldPointPos.x - ballPos.x, worldPointPos.y - ballPos.y, worldPointPos.z - ballPos.z);
-        _cue.transform.rotation = Quaternion.LookRotation(vectorForRotation, Vector3.up);
-
+        _cue.rotation = Quaternion.LookRotation(vectorForRotation, Vector3.up);
        
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             var vectorForForce = new Vector3(worldPointPos.x - ballPos.x, 0, worldPointPos.z - ballPos.z);
-            _rbBall.AddForce(vectorForForce.normalized * _force, ForceMode.Impulse);
+            _ball.AddForce(vectorForForce.normalized * _force, ForceMode.Impulse);
         }
     }
 }
